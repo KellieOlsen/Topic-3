e3bb2cada80551012c4af9d58f20c355edabf6c7
Kellie Olsen <kellienstreet@comcast.net>
2017-04-30 22:48:00 -0500

    Add files via upload

---
+++ SixFive.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SixFive {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter three numbers: ");
+		double num1 = input.nextDouble();
+		double num2 = input.nextDouble();
+		double num3 = input.nextDouble();
+		displaySortedNumbers(num1, num2, num3);
+	}
+		
+	public static void displaySortedNumbers(double num1, double num2, double num3) {
+	
+		if ((num1 > num2) && (num2 > num3)) {
+			System.out.println("The sorted numbers are: " + num1 + " " + num2 + " " + num3);
+		}
+		else if ((num1 > num2) && (num3 > num2)) {
+			System.out.println("The sorted numbers are: " + num1 + " " + num3 + " " + num2);
+		}
+		else if ((num2 > num1) && (num1 > num3)) {
+			System.out.println("The sorted numbers are: " + num2 + " " + num1 + " " + num3);
+		}
+		else if ((num2 > num1) && (num3 > num2)) {
+			System.out.println("The sorted numbers are: " + num2 + " " + num3 + " " + num1);
+		}
+		else if ((num3 > num2) && (num2 > num1)) {
+			System.out.println("The sorted numbers are: " + num3 + " " + num2 + " " + num1);
+		}
+		else if ((num3 > num2) && (num1 > num2)) {
+			System.out.println("The sorted numbers are: " + num3 + " " + num1 + " " + num2);
+
+		}
+	}
+}
